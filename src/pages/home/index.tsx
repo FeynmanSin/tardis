32c396c8d8e56a2bfe0f33446c8fc56429febd6e
feynmansin <feynman.sin@gmain.com>
2023-12-20 04:12:22 +0800

    feat: 虚拟列表

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,42 +1,16 @@
-import React, { useContext, useEffect, useCallback, useState, useRef } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { AppContext } from '@/layouts';
-
-const A = ({ onChange }) => {
-  console.log(">>>>>>>a render")
-  const change = (e) => {
-    onChange(e.target.value);
-  }
-
-  return (
-    <input type="text" onChange={change} />
-  )
-}
-
-
-
 const Home: React.FC = () => {
   const context = useContext(AppContext);
-  const [count, setCount] = useState(0);
+
 
   useEffect(() => {
     context.siderHandler(false);
   }, [])
 
-  const change = () => {
-    console.log(">>>>count", count)
-    setCount(count + 1)
-  }
-  console.log(">>>>count3", count)
-  const change2 = useCallback(() => {
-    console.log(">>>>count2", count)
-    setCount(count + 1)
-  }, [])
 
   return (
-    <div>Home
-      <div>{count}</div>
-      <A onChange={change2} />
-    </div>
+    <div>Home</div>
   )
 }
 
